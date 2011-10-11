f24205ea802aed5c0537382276ab73c2ed5e0491
Kenta Suzuki <suzuken326@gmail.com>
2011-10-11 18:01:32 +0900

    modify output format for namespace

--- src/rdfmaker/RDFMaker.java
+++ src/rdfmaker/RDFMaker.java
@@ -43,11 +43,11 @@ import xbrlreader.XbrlReader;
  * @author suzuken
  */
 public class RDFMaker implements Maker {
-	
+
 	public XbrlReader x;
 	public String dest;
 	public Model model;
-	
+
 	private String nsCls;
 	private String nsIns;
 	private String nsPrp;
@@ -58,24 +58,23 @@ public class RDFMaker implements Maker {
 	private String outputRDFDir;
 
 	private String[] enableContextRef;
-	
-	
+
+
 	//for testing
 	// 0: xbrl's url
 	// 1: destination folder or output filename's path
 	public static void main(String[] args) throws TransformerException, SAXException, IOException, ParserConfigurationException, XPathExpressionException{
 		long start = System.currentTimeMillis();
-		
+
 		RDFMaker r = new RDFMaker(args);
 		r.createModel();
-		r.model.write(System.out);
-		
-		//取得
+		r.outputRDF(r.model);
+
 		long stop = System.currentTimeMillis();
-		
+
 		System.out.println("実行時間は" + (stop - start) + "ミリ秒です。");
 	}
-	
+
 	/**
 	 * ローカルファイルにrdfを出力する関数。
 	 * 
@@ -100,13 +99,13 @@ public class RDFMaker implements Maker {
 		model.write(out);
 	}
 
-	
+
 	public RDFMaker(String args[]) throws TransformerException, SAXException, IOException, ParserConfigurationException{
 		super();
 		this.x = new XbrlReader(args[0]);
 		this.x.prepare();
 		this.dest = args[1];
-		
+
 		String configFile = "app.conf"; // for default
 		Properties prop = new Properties();
 		try {
@@ -119,8 +118,8 @@ public class RDFMaker implements Maker {
 
 		String[] buffer = prop.getProperty("enableContextRef").split("\\,");
 		String[] contexts = new String[buffer.length];
- 		for(Integer i =0; i < buffer.length; i++){
- 			contexts[i] = buffer[i].trim();	//先頭及び最後の空白を取り除く
+		for(Integer i =0; i < buffer.length; i++){
+			contexts[i] = buffer[i].trim();	//先頭及び最後の空白を取り除く
 		}
 		this.setEnableContextRef(contexts);
 		this.setNsCls(prop.getProperty("nsClass"));
@@ -131,16 +130,16 @@ public class RDFMaker implements Maker {
 		this.setOutputRDFDir(prop.getProperty("outputRDFDir"));
 		this.setOutputRDFPath(prop.getProperty("outputRDFDir") + getFileName(args[0]) + ".rdf");
 	}
-	
 
-	
+
+
 	/**
 	 *　modelを作成するメソッド 
 	 * @throws XPathExpressionException 
 	 */
 	public void createModel() throws XPathExpressionException{
 		this.model = TDBFactory.createModel();
-		
+
 		Resource company = this.model.createResource(this.getNsIns() + this.x.getDocumentInfo("EntityNameJaEntityInformation").getValue());
 		Resource foafOrg = this.model.createResource(this.getNsFoaf()
 				+ "Organization");
@@ -150,84 +149,79 @@ public class RDFMaker implements Maker {
 		Resource edicode = this.model.createResource(this.getNsIns()
 				+ this.x.getContext("DocumentInfo").getIdentifier());
 		Property hasEdicode = this.model.createProperty(this.getNsPrp(),
-				"hasEDINETCode");
+		"hasEDINETCode");
 		company.addProperty(hasEdicode, edicode);
 
 		// jpfr-t-<?> hasAccountをつくる
 		Resource accountElement= null;
-		
+
 		//ContextごとにRDF modelを生成
 		for(Integer j = 0; j < this.getEnableContextRef().length; j++){
 			String contextRef = this.getEnableContextRef()[j];
 			if(contextRef != null){
 
 				ArrayList<Account> a = this.x.getAccountsByContext(contextRef);
-//				ArrayList<Account> a = this.x.getAccountsByContext("Prior1YearNonConsolidatedDuration");
 				if(!(a.isEmpty())){
-					for (int i = 0; i < a.size(); i++){
-						for (Account account : a){
-							accountElement = this.model.createResource(account.getNamespaceURI()
-									+ account.getLocalName());
-							Property hasAccount = this.model.createProperty(this.getNsIns(), "hasAccount");
-							edicode.addProperty(hasAccount, accountElement);
-							
-							// accountElementにvalueをつける。hasAmountプロパティで。
-							Resource blankValue = this.model.createResource();
-							Property hasAmount = this.model.createProperty(this.getNsIns(),
-									"hasAmount");
-							accountElement.addProperty(hasAmount, blankValue);
+					for (Account account : a){
+						accountElement = this.model.createResource(account.getNamespaceURI()
+								+ "#" + account.getLocalName());
+						Property hasAccount = this.model.createProperty(this.getNsIns(), "hasAccount");
+						edicode.addProperty(hasAccount, accountElement);
 
-							// 空白ノードに金額情報をつける。数値はリテラルで、型はxsd:integer
-							Literal valueOfAccount = this.model.createTypedLiteral(
-									(account.getValue() != null) ? account.getValue() : 0,
-									XSD.integer.getURI());
-							blankValue.addLiteral(RDF.value, valueOfAccount);
-							
-							// 金額情報の通貨単位をつける iso4217:JPYとか。
-							Unit unit = this.x.getUnit(account.getUnitRef());
-							Resource unitOfAccount = this.model.createResource(
-									unit.getMeasureNamespaceURI() + "#" + unit.getMeasureLocalName());
-							Property hasUnit = this.model.createProperty(this.getNsIns(), "hasUnit");
-							blankValue.addProperty(hasUnit, unitOfAccount);
+						// accountElementにvalueをつける。hasAmountプロパティで。
+						Resource blankValue = this.model.createResource();
+						Property hasAmount = this.model.createProperty(this.getNsIns(),
+						"hasAmount");
+						accountElement.addProperty(hasAmount, blankValue);
 
-							// 日付情報を付属させる。そのエレメントが属するクラスによって変わってくる。
-							// contextInfoからとる。contextRefから参照できるcontextInfoの、periodInstantまたはstartDate,endDateをとってくる。
-							// contextInfoのidによってクラスをInstantとDurationに分けてあるので、それぞれからとるようにする。
-							Context c = this.x.getContext(contextRef);
-							if(c.getPeriodInstant() != null){
-								Literal Instant = this.model.createTypedLiteral(c.getPeriodInstant(), 
-										XSDDateType.XSDdate);
-								Property hasInstant = this.model.createProperty(this.getNsIns(),
-										"hasInstant");
-								accountElement.addLiteral(hasInstant, Instant);
-							}
-							else if(c.getPeriodStartDate() != null){
-								Literal startDate = this.model.createTypedLiteral(c.getPeriodStartDate(),
-										XSDDateType.XSDdate);
-								Literal endDate = this.model.createTypedLiteral(c.getPeriodEndDate(),
-										XSDDateType.XSDdate);
-								Property hasStartDate = this.model.createProperty(this.getNsIns(),
-										"hasStartDate");
-								Property hasEndDate = this.model.createProperty(this.getNsIns(),
-										"hasEndDate");
-								accountElement.addLiteral(hasStartDate, startDate);
-								accountElement.addLiteral(hasEndDate, endDate);
-							}
-							else{
-								System.out.println("periodの指定が不正です。");
-							}
+						// 空白ノードに金額情報をつける。数値はリテラルで、型はxsd:integer
+						Literal valueOfAccount = this.model.createTypedLiteral(
+								(account.getValue() != null) ? account.getValue() : 0,
+										XSD.integer.getURI());
+						blankValue.addLiteral(RDF.value, valueOfAccount);
 
+						// 金額情報の通貨単位をつける iso4217:JPYとか。
+						Unit unit = this.x.getUnit(account.getUnitRef());
+						Resource unitOfAccount = this.model.createResource(
+								unit.getMeasureNamespaceURI() + "#" + unit.getMeasureLocalName());
+						Property hasUnit = this.model.createProperty(this.getNsIns(), "hasUnit");
+						blankValue.addProperty(hasUnit, unitOfAccount);
+
+						// 日付情報を付属させる。そのエレメントが属するクラスによって変わってくる。
+						Context c = this.x.getContext(contextRef);
+						if(c.getPeriodInstant() != null){
+							Literal Instant = this.model.createTypedLiteral(c.getPeriodInstant(), 
+									XSDDateType.XSDdate);
+							Property hasInstant = this.model.createProperty(this.getNsIns(),
+									"hasInstant");
+							accountElement.addLiteral(hasInstant, Instant);
 						}
+						else if(c.getPeriodStartDate() != null){
+							Literal startDate = this.model.createTypedLiteral(c.getPeriodStartDate(),
+									XSDDateType.XSDdate);
+							Literal endDate = this.model.createTypedLiteral(c.getPeriodEndDate(),
+									XSDDateType.XSDdate);
+							Property hasStartDate = this.model.createProperty(this.getNsIns(),
+									"hasStartDate");
+							Property hasEndDate = this.model.createProperty(this.getNsIns(),
+							"hasEndDate");
+							accountElement.addLiteral(hasStartDate, startDate);
+							accountElement.addLiteral(hasEndDate, endDate);
+						}
+						else{
+							System.out.println("periodの指定が不正です。");
+						}
+
 					}
 				}
 			}
 		}
 	}
-	
+
 	public Model getModel(){
 		return this.model;
 	}
-	
+
 	public String getNsCls() {
 		return nsCls;
 	}
@@ -290,7 +284,7 @@ public class RDFMaker implements Maker {
 	public void setEnableContextRef(String[] enableContextRef) {
 		this.enableContextRef = enableContextRef;
 	}
-	
+
 	/**
 	 * ファイル名から拡張子を取り除く
 	 * @param filename
@@ -307,5 +301,5 @@ public class RDFMaker implements Maker {
 		}
 		return filename;
 	}
-	
+
 }
